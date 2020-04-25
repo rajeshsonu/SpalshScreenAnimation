8f70d208df159f76fbb6c3b083354982019fed9c
rajeshn <rajeshn@yupptv.com>
2020-04-25 22:43:01 +0530

    Initial Commit

---
+++ admission24/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  admission24
+//
+//  Created by Rajesh Nekkanti on 25/04/20.
+//  Copyright © 2020 Rajesh Nekkanti. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
